d6ca833ec8495b0cab5862d897b61a44116e68d9
Sebastian Ohl <sebastian@ohl.name>
2020-11-17 18:41:53 +0100

    move to vscode

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#export EDF_HOME=~/esp/esp-idf/
+export IDF_PATH=~/esp/esp-idf/
+export ESPBAUD=921600
+export ESPPORT=/dev/ttyUSB0
+#export PATH=~/esp/xtensa-lx106-elf/bin/:$PATH
+export python=python3
+
+. $IDF_PATH/export.sh
